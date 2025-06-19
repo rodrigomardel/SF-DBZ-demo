3ecb59626e1346fafb999a9585cfbd7c4aca15a9
rodrigomardel <rodrigo.mardel.daw@gmail.com>
2025-06-19 12:01:03 +0200

    [FEAT] Integración CRUD Personaje

--- src/Controller/Admin/PersonajeCrudController.php
+++ src/Controller/Admin/PersonajeCrudController.php
@@ -7,6 +7,13 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Form\Type\FileUploadType;
+
+
 
 class PersonajeCrudController extends AbstractCrudController
 {
@@ -15,14 +22,17 @@ class PersonajeCrudController extends AbstractCrudController
         return Personaje::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(), 
+            TextField::new('name', 'Nombre'),
+            TextEditorField::new('description', 'Descripción')->setRequired(false),
+            TextField::new('image', 'URL Imagen'),
+            DateTimeField::new('deletedAt', 'Fecha de eliminación')->setRequired(false),
+            AssociationField::new('planeta', 'Planeta') 
+            ->setFormTypeOption('choice_label', 'name')  
+            ->setRequired(true),
         ];
     }
-    */
 }
